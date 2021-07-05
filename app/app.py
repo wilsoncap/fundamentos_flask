179474c387772c41aef0f3bfc1a9ed9ab875fb89
Wilson Capacho <Wiston.98@hotmail.com>
2021-07-04 22:30:11 -0500

    ultimos cambios

--- app/app.py
+++ app/app.py
@@ -1,6 +1,26 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect, jsonify
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+
+#conexion a MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'prueba_21'
+
+conexion = MySQL(app)
+
+@app.before_request
+def before_request():
+  print('Antes de la peticion....')
+
+@app.after_request
+def after_request(response):
+  print('Despues de la peticion')
+  return response
+
+
 
 @app.route('/')#decorador
 def index():#ruta esta enlazada con el decorador
@@ -25,15 +45,45 @@ def contacto(nombre, edad):
   return render_template('contacto.html', data=data)
 ##"""
 
-def query_string():
+def query_string():#query_string?param1=Wilson&param2=33
   print(request)# objeto requestr , dentro del modelo C/S este es la peticion que se le hace al servidor
   print(request.args)
   print(request.args.get('param1'))
   print(request.args.get('param2'))
-  return 'Ok Wilson vas bien'
+  p1 = request.args.get('param1')
+  p2 = request.args.get('param2')
+  data={
+    'arg1': p1,
+    'arg2': p2
+
+  }
+  return render_template('query_string.html', data=data)
+
+@app.route('/cursos')
+def listar_cursos():
+  data = {}
+
+  try:
+    cursor = conexion.connection.cursor()
+    sql = 'SELECT Coprod, Nomcurso, Credcurso FROM cursos ORDER BY Nomcurso ASC'
+    cursor.execute(sql)
+    cursos = cursor.fetchall()
+    print(cursos)
+    data['cursos'] = cursos
+    data['mensaje'] = 'Exitoso en la conexion'
+  except Exception as ex:
+    data['mensaje'] ='Error...'
+  return jsonify(data)
+
+
+@app.route('/mostrarcursos')
+def show_cursos():
+  #cursos = get('http//127.0.0.1:5000/cursos').json()
+
 
 def pagina_no_encontrada(error):
-  return render_template('404.html'), 404
+  # return render_template('404.html'), 404
+  return redirect(url_for('index'))
 
 
 if __name__=='__main__':
@@ -41,15 +91,22 @@ if __name__=='__main__':
   app.register_error_handler(404, pagina_no_encontrada)
   app.run(debug=True, port=5000)
 
-# Virtualenv -p python3 env
-# pip list
+
+# py -m pip install virtualenv  --> para crear entorno virtual globalmente
+# Virtualenv -p python3 env, otro_nombre  --> para crear entorno virtual localmente
+# pip list -->para ver lo que hay en la ruta actual
+# pip install flask --> para insalar flask
 # .\env\Scripts\Activate --> para entrar al entorno virual donde podemos instalar paquetes, librerias y framework
 # deactivate --> para salir del entorno virtual
 # pip freeze > requirements.txt --> crea un archivo con todos los paquetes del proyecto para que se puedan migrar a otro proyecto
 # pip install -r requirements.txt --> para instalar los paquetes en otro proyecto
-# python .\app\app.py
+# python .\app\app.py --> se ejecuta en el entorno virtual
 # python.exe pip install --upgrade pip
 # python .\src\app.py
 # pip list --outdated --> para ver los paquetes que estan desactualizados
 # pip show "nombre del paquete" --> para ver toda la informacion mas detallada del paquete
 # pip check "nombre del paquete" --> sirve para ver si el paquete tiene algun faltante de otra dependencia o estas esten rotas
+# pip install flask-sqlalchemy --> modulo para comunicarse con base de datos
+# pip install flask-marshmallow --> modulo para definir squemas en nuestra base de datos
+# pip install marshmallow-sqlalchemy --> librerias que trabajan entre ellas mismas
+# pip install pymysql -->modulo para poder crear conexion con la base de datos
